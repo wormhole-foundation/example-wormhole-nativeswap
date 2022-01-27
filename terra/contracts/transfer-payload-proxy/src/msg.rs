47a6f35a83d479ccd8dfc817186a92bd68cce3f3
Csongor Kiss <ckiss@jumptrading.com>
2022-01-27 18:25:50 +0000

    snake case in messages

--- terra/contracts/transfer-payload-proxy/src/msg.rs
+++ terra/contracts/transfer-payload-proxy/src/msg.rs
@@ -9,12 +9,14 @@ type HumanAddr = String;
 pub struct MigrateMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
 pub struct InstantiateMsg {
     pub token_bridge_contract: HumanAddr,
     pub wormhole_contract: HumanAddr,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
     RedeemPayload { data: Binary },
 }
