dd1185266f4d4340828c1babf63e0dbf278d3709
Evan Gray <battledingo@gmail.com>
2022-04-08 14:35:38 +0000

    attempt cross-call

--- solana/src/api/complete_transfer_and_swap.rs
+++ solana/src/api/complete_transfer_and_swap.rs
@@ -3,37 +3,34 @@ use bridge::{
 };
 use token_bridge::{
     accounts::*,
-    types::*,
+    api::{
+        complete_transfer::{
+            CompleteWrappedData,
+        },
+    },
+    instruction::Instruction::CompleteWrapped,
     messages::{
         PayloadTransferWithPayload,
-    }
+    },
+    types::*
 };
 use solana_program::{
     account_info::AccountInfo,
+    instruction::{
+        AccountMeta,
+        Instruction,
+    },
     msg,
     program::invoke_signed,
-    // program_error::ProgramError,
     pubkey::Pubkey,
 };
 use solitaire::{
     processors::seeded::{
-    //     invoke_seeded,
         Seeded,
     },
     CreationLamports::Exempt,
     *,
 };
-// use spl_token::state::{
-//     Account,
-//     Mint,
-// };
-// use std::ops::{
-//     Deref,
-//     DerefMut,
-// };
-
-// pub type ConfigAccount<'b, const State: AccountState> =
-//     Derive<Data<'b, Config, { State }>, "config">;
 
 pub type TokenBridgeConfigAccount<'b, const STATE: AccountState> = Data<'b, Config, { STATE }>;
 
@@ -144,6 +141,37 @@ pub fn complete_transfer_and_swap(
         )?;
         invoke_signed(&init_ix, ctx.accounts, &[])?;
     }
+
+    // see https://github.com/certusone/wormhole/blob/2e24f11fa045ac8460347d9796a4ecdb7931a154/solana/modules/token_bridge/program/src/instructions.rs#L312-L338
+    // TODO: maybe there's a better way to rebuild this off our list of accounts which should be nearly compatible
+    let bridge_id = ctx.accounts[14].info().key;
+    let message_key = ctx.accounts[2].info().key;
+    msg!("bridge_id: {:?}", bridge_id);
+    msg!("message_key: {:?}", message_key);
+    let transfer_ix = Instruction {
+        program_id: *accs.token_bridge.info().key,
+        accounts: vec![
+            AccountMeta::new(*accs.payer.info().key, true),
+            AccountMeta::new_readonly(*accs.config.info().key, false),
+            AccountMeta::new_readonly(*message_key, false),
+            AccountMeta::new(*accs.vaa.claim.info().key, false),
+            AccountMeta::new_readonly(*accs.chain_registration.info().key, false),
+            AccountMeta::new(*accs.custody.info().key, false),
+            AccountMeta::new_readonly(*accs.custody_signer.info().key, true),
+            AccountMeta::new(*accs.to_fees.info().key, false),
+            AccountMeta::new(*accs.mint.info().key, false),
+            AccountMeta::new_readonly(*accs.wrapped_meta.info().key, false),
+            AccountMeta::new_readonly(*accs.mint_authority.info().key, false),
+            // Dependencies
+            AccountMeta::new_readonly(solana_program::sysvar::rent::id(), false),
+            AccountMeta::new_readonly(solana_program::system_program::id(), false),
+            // Program
+            AccountMeta::new_readonly(*bridge_id, false),
+            AccountMeta::new_readonly(spl_token::id(), false),
+        ],
+        data: (CompleteWrapped, CompleteWrappedData {}).try_to_vec()?,
+    };
+    invoke_signed(&transfer_ix, ctx.accounts, &[])?;
     
     Ok(())
 }
