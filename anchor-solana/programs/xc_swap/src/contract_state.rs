5f8a8cbe0175b87fc343d5d9afdcec89ac6cb9a6
skojenov <sekoje@users.noreply.github.com>
2022-06-30 21:19:21 +0000

    Added wormhole PDAs to contract_state for verification.

--- anchor-solana/programs/xc_swap/src/contract_state.rs
+++ anchor-solana/programs/xc_swap/src/contract_state.rs
@@ -5,9 +5,18 @@ use anchor_lang::prelude::*;
 pub struct ContractState {
     pub wormhole_pubkey: Pubkey,
     pub token_bridge_pubkey: Pubkey,
+
+    // Wormhole core and token bridge PDA we need to verify.
+    pub custody_signer_key: Pubkey,     // 32
+    pub mint_signer_key: Pubkey,     // 32
+    pub authority_signer_key: Pubkey,     // 32
+    pub bridge_config_key: Pubkey,     // 32
+    pub wormhole_config_key: Pubkey,     // 32
+    pub fee_collector_key: Pubkey,     // 32
+    pub wormhole_emitter_key: Pubkey,     // 32
+    pub wormhole_sequence_key: Pubkey,     // 32
 }
 
 impl ContractState {
-    pub const MAXIMUM_SIZE: usize = 32 + 32;
-
+    pub const MAXIMUM_SIZE: usize = 32 + 32 + 32*8;
 }
